ce146af33ecf9b9320e548910dd216697a9a4b44
Anton Stephanyak <anton.stephanyak@gmail.com>
2020-05-04 14:03:53 +0500

    [del] Test components deleted [fix] App files adjusted for deleted components

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -6,14 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServicesModule } from './services/services.module';
 
 import { AppComponent } from './app.component';
-import { AuthTestComponent } from './components/auth-test/auth-test.component';
-import { WebsocketsTestComponent } from './components/websockets-test/websockets-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthTestComponent,
-    WebsocketsTestComponent,
   ],
   imports: [
     // Angular modules
