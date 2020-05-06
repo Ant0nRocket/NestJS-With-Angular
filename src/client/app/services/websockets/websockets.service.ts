5345cfb16c4029031802e008fa25c2282b107689
Anton Stephanyak <anton.stephanyak@gmail.com>
2020-05-06 18:18:43 +0500

    [fix] Code refactoring for WebSockets service [feat] WebSocketOptions could be passed to connect method

--- src/client/app/services/websockets/websockets.service.ts
+++ src/client/app/services/websockets/websockets.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable, EventEmitter } from '@angular/core';
+import { takeWhile } from 'rxjs/operators';
 
 import { ServicesModule } from '../services.module';
-import { apiConfig } from '../../../../shared/api.config';
-import { environment } from '../../../environments/environment';
+import { WebSocketOptions } from './websocket.options';
+import { AuthService } from '../auth/auth.service';
+
 import { WebSocketsDto } from '../../../../shared/websockets/websockets.dto';
 import { WebSocketsTheme } from '../../../../shared/websockets/websockets-theme.enum';
 
@@ -15,30 +16,11 @@ export class WebSocketsService {
   /** Instance of WebSocket */
   private ws: WebSocket;
 
-  /** Connection URL */
-  private url: string;
+  /** Connection options */
+  private options: WebSocketOptions = new WebSocketOptions();
 
-  /** 
-   * For exanple, Heroku closes any connection in 30 sec. 
-   * if there were no packages transmited. 
-   * Below there is Ping-Pong code which fire every 25000 ms.
-   */
-  private pingPongInterval = 20000;
-
-  /** 
-   * Set to true if you need to start reconnection procedure
-   * on error or close
-   */
-  private reconnectOnClose = true;
-
-  /** 
-   * If WebSocket closed because of some error it will
-   * try to reconnect every 5 seconds
-   */
-  private reconnectionOnErrorInterval = 5000;
-
-  /** Max amount of reconnection attempts */
-  private reconnectionAttemptsLimit = 10;
+  /**  */
+  private intervPingPong: number;
 
   /** Currect reconnection attempts count */
   private reconnectionAttemptsPassed = 0;
@@ -48,7 +30,7 @@ export class WebSocketsService {
    * attempts after some amout fails 'cause of memory leaks
    */
   private get reconnectionAttemptsLimitReached(): boolean {
-    return this.reconnectionAttemptsPassed >= this.reconnectionAttemptsLimit;
+    return this.reconnectionAttemptsPassed >= this.options.reconnectionAttemptsLimit;
   }
 
   /** Whether websocket open or not */
@@ -56,44 +38,49 @@ export class WebSocketsService {
     return this.ws?.readyState === WebSocket.OPEN;
   }
 
-  public onMessageReceived$: Subject<WebSocketsDto> = new Subject();
+  /** Emits when there is new message from server */
+  public onMessageReceived$: EventEmitter<WebSocketsDto> = new EventEmitter();
 
-  constructor() {
-    // if your you have HTTPS connection then (at least Firefox) will
-    // block attempts to connect to ws:// resources because they are
-    // "insecure connection"'s. In that case you should provide wss:// url
-    const wsProtocolPrefix = location.protocol.startsWith('https') ? 'wss://' : 'ws://';
-    this.url = wsProtocolPrefix + location.host + apiConfig.urlWebSocket;
-
-    if (!environment.production) {
-      // in dev mode backend usualy on 3000 port, so let's change it
-      this.url = this.url.replace('4200', '3000');
-    } else {
-      // in production mode it's a good idea to ping-pong server sometimes ;)
-      setInterval(() => { this.send() }, this.pingPongInterval);
-    }
-  }
+  constructor(private authService: AuthService) { }
 
   /** 
    * Connects to WebSocket server. 
    * If there is an open connection then nothing will happen.
    */
-  public connect() {
+  public connect(options?: WebSocketOptions, onOpenCallback?: () => void) {
     if (this.ready) return;
-    this.reconnectOnClose = true;
+    if (options) this.options = options;
 
-    this.ws = new WebSocket(this.url);
+    //----------
 
+    this.ws = new WebSocket(this.options.url);
     this.ws.onopen = () => {
       this.reconnectionAttemptsPassed = 0;
+      if (onOpenCallback) onOpenCallback();
+
+      this.intervPingPong =
+        window.setInterval(() => { this.send() }, this.options.pingPongIntervalMs);
+
+      this.authService.onAuthStateChanged$.pipe( // on authToken = null -> close ws
+        takeWhile(state => state === true)
+      ).subscribe(null, null, /*complete*/() => {
+        this.ws?.close();
+      })
     };
+
+
+    //----------
 
     this.ws.onmessage = (ev) => { // received message
       try {
         const data: WebSocketsDto = JSON.parse(ev.data);
-        this.onMessageReceived$.next(data);
+        if (data.theme === WebSocketsTheme.ClientConnected) {
+          this.sendAuthToken();
+        } else {
+          this.onMessageReceived$.emit(data);
+        }
       } catch {
-        this.onMessageReceived$.next({
+        this.onMessageReceived$.emit({
           cid: '',
           theme: WebSocketsTheme.BadDto,
           content: ev.data
@@ -101,26 +88,41 @@ export class WebSocketsService {
       }
     };
 
+    //----------
+
     this.ws.onclose = () => {
-      if (this.reconnectOnClose) {
+      window.clearInterval(this.intervPingPong);
+
+      if (this.options.reconnectOnClose) {
         setTimeout(() => {
           if (!this.reconnectionAttemptsLimitReached && !this.ready) {
             this.reconnectionAttemptsPassed++;
             this.connect();
           }
-        }, this.reconnectionOnErrorInterval);
+        }, this.options.reconnectionOnErrorInterval);
       }
     };
+  }
+
+  /** Sends provided JWT token DTO to server */
+  private sendAuthToken() {
+    const dto: WebSocketsDto = {
+      cid: '',
+      theme: WebSocketsTheme.AuthenticateWithToken,
+      content: this.authService.authToken
+    };
+    this.send(dto);
   }
 
   /** 
    * Closes connection to server (if it's in OPEN state) and blocks
    * reconnection by setting reconnectOnClose to false.
    */
-  close() {
-    this.reconnectOnClose = false;
+  public close() {
+    const oldOptionReconnectOnClose = this.options.reconnectOnClose;
+    this.options.reconnectOnClose = false;
     this.ws?.close();
-    console.log('WebSocket connection closed on demand. No reconnection scheduled.');
+    this.options.reconnectOnClose = oldOptionReconnectOnClose;
   }
 
   /** 
@@ -129,7 +131,7 @@ export class WebSocketsService {
    * If no WebSocketsDto provided then empty string will be sent.   
    * It can be useful in Ping-Pong algorythm.
    */
-  send(m?: WebSocketsDto | string) {
+  public send(m?: WebSocketsDto | string) {
     if (!this.ready) return;
     if (!m) m = '';
     const message2send = typeof m === 'string' ? m : JSON.stringify(m);
